19b28dc29fe7b44783d379612f37f3588faacfb2
JuliMelnikova <ulia682082@gmail.com>
2023-01-21 23:06:14 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,68 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryImages = galleryItems.map(imgOptions => {
+    return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${imgOptions.original}">
+                <img
+                    class="gallery__image"
+                    src="${imgOptions.preview}"
+                    data-source="large-image.jpg"
+                    alt="${imgOptions.description}"
+                >
+            </a>
+        </div>
+    `;
+}).join('');
+
+galleryEl.innerHTML = galleryImages;
+
+galleryEl.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(
+        `<img src="${event.target.src}">
+    `,
+    
+    {
+        onShow: (instance) => {
+            document.addEventListener("keydown", escape);
+        },
+        onClose: (instance) => {
+            document.removeEventListener("keydown", escape);
+        },
+        }
+    );
+    instance.show();
+
+    function escape(e) {
+        console.log('ESCAPE', e);
+        if (e.code === 'Escape') {
+        instance.close();
+        }
+    }
+    }
+);
+    
+/*
+// 	 * Prevents the lightbox from closing when clicking its background.
+// 	 */
+// closable: true,
+// /*
+//  * One or more space separated classes to be added to the basicLightbox element.
+//  */
+// className: '',
+// /*
+//  * Function that gets executed before the lightbox will be shown.
+//  * Returning false will prevent the lightbox from showing.
+//  */
+// onShow: (instance) => {},
+// /*
+//  * Function that gets executed before the lightbox closes.
+//  * Returning false will prevent the lightbox from closing.
+//  */
+// onClose: (instance) => {}
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryImages = galleryItems.map(imgOptions => {
+    return `
+            <a class="gallery__item" href="${imgOptions.original}">
+                <img
+                    class="gallery__image"
+                    src="${imgOptions.preview}"
+                    alt="${imgOptions.description}"
+                >
+            </a>
+    `;
+}).join('');
+
+galleryEl.innerHTML = galleryImages;
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', 
+    captionPosition: 'bottom', 
+    captionDelay: 250, 
+    // animationSpeed: 10,
+    // fadeSpeed: 100
+});
